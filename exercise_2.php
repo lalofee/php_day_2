a8317049193ebc740c5606f19bd02d9692d4f076
Angela Armstorfer <armstorfer@silverserver.at>
2018-02-07 09:14:21 +0100

    exercise 2

--- exercise_2.php
+++ exercise_2.php
@@ -7,39 +7,53 @@
 
 <body>
 	
-	<form action="exercise_2.php" method="POST">
-
- 		Name: <input type="text" name="name" />
-
- 		Surname: <input type="text" name="surname" />
-
- 		<input type="submit" name="submit" />
-
- 	</form>
-
 <?php
+
+// define variables and set to empty values
+$nameErr = $surnameErr = "";
+$name = $surname = "";
 	
-if(isset($_POST['submit']))
 
-{
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    echo "Hallo ". ($_POST["name"]);
+    
+    }
+  }
 
+  if (empty($_POST["surname"])) {
+    $surnameErr = "Surname is required";
+  } else {
+    echo " " . ($_POST["surname"]);
+     
+    }
+  
 
- if( $_POST["name"] || $_POST["surname"] )
-
- {
-echo "Welcome ". $_POST['name']; 
-echo " ". $_POST['surname'];
- }
-
-}
-
-
+  
 ?>
 
 
-	
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
-<br>
+Name: <input type="text" name="name">
+<span class="error">* <?php echo $nameErr;?></span>
+<br><br>
+Surname:
+<input type="text" name="surname">
+<span class="error">* <?php echo $surnameErr;?></span>
+<br><br>
+<input type="submit" name="submit" value="Submit"> 
+
+
+</form>
+
+
+
+ 
+
+
 
 
 </body>
