e9e07d4761f1c1b7eed0bc601359e609f10e82bf
Angela Armstorfer <armstorfer@silverserver.at>
2018-02-07 09:48:25 +0100

    exercise 3

---
+++ exercise_3.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="style.css">
+	<title>integer parameters</title>
+</head>
+
+<body>
+	
+<?php
+//Create a function which takes two integer parameters - divide them and output result on the screen.
+
+function divide() {
+	$a = 10;
+	$b = 2;
+	$ergebnis = $a / $b;
+	echo $ergebnis;
+}
+divide();
+
+?>
+
+
+
+
+
+ 
+
+
+
+
+</body>
+</html>
