2dc35c74d7005c9ec09898f235bdad0f150cad04
Angela Armstorfer <armstorfer@silverserver.at>
2018-02-07 10:02:40 +0100

    exercise 4

---
+++ exercise_4.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="style.css">
+	<title>create database</title>
+</head>
+
+<body>
+	
+<?php
+//Create Database using PHP and MySQL.
+
+//connect to mysql
+$servername = "localhost";
+	$user = "root";
+	$pw = "";
+	$dbname = "my_test_database";
+
+	$con = new mysqli($servername, $user, $pw);
+
+	if($con ->connect_error) {
+		die("alles kaputt".$con ->connect_error);
+	}
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($con->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $con->error;
+}
+
+$con->close();
+
+
+
+
+?>
+
+</body>
+</html>
+
